4668760d277350ee4e91fea71553b02d1e6c459f
High-Jay <josephisrael563@gmail.com>
2021-05-28 12:37:46 +0100

    axios update

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "http://localhost:9000",
+    baseURL: "http://free-eth-backend.herokuapp.com",
 });
 
 export default instance;
